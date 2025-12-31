84b6454c021c76dd344bbb69a631be9984350d4b
Douglas Dreer <douglasdreer@outlook.com.br>
2025-12-31 12:17:53 -0300

    Add version list generation methods to VersionFixture

--- src/test/kotlin/io/github/riotgames/leagueoflegends/support/fixtures/VersionFixtures.kt
+++ src/test/kotlin/io/github/riotgames/leagueoflegends/support/fixtures/VersionFixtures.kt
@@ -5,11 +5,38 @@ import io.github.riotgames.leagueoflegends.domain.model.Version
 object VersionFixture {
     fun valid(
         id: Long? = null,
-        number: String = "13.6.1",
-        isCurrent: Boolean = false
+        number: String = "13.6.1"
     ) = Version(
         id = id,
-        number = number,
-        isCurrent = isCurrent
+        number = number
     )
+
+    fun createVersionList(size: Int = 1): List<Version> {
+        return (1..size).map { index ->
+            valid(
+                id = index.toLong(),
+                number = "13.6.${index - 1}"
+            )
+        }
+    }
+
+    fun versionList(): List<String> {
+        return listOf(
+            "13.6.1",
+            "13.6.0",
+            "13.5.1",
+            "13.5.0",
+            "13.4.1",
+            "13.4.0",
+            "13.3.1",
+            "13.3.0",
+            "13.2.1",
+            "13.2.0",
+            "13.1.1",
+            "13.1.0",
+            "13.0.1",
+            "13.0.0"
+        )
+}
+
 }
